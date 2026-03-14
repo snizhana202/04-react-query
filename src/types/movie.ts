b2a6a813cf21194acdd440085c8e63a66a6e2d7e
Snizhana <snizhanapetrushka@gmail.com>
2026-03-14 14:54:58 +0200

    adding smth to Movie

--- src/types/movie.ts
+++ src/types/movie.ts
@@ -3,6 +3,9 @@ export interface Movie {
   title: string;
   overview: string;
   poster_path: string;
+  backdrop_path?: string | null;
+  release_date?: string;
+  vote_average?: number;
 }
 
 export interface MoviesResponse {
