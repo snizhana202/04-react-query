d05d171a5f67b4475f3f708b0712ee7216d6dccf
Snizhana <snizhanapetrushka@gmail.com>
2026-03-14 19:43:56 +0200

    deleted doubled files

--- src/services/movieService.ts
+++ src/services/movieService.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 import type { MoviesResponse } from "../types/movie";
 
-const MY_API_KEY = import.meta.env.API_KEY;
+const API_KEY = import.meta.env.VITE_TMDB_TOKEN;
 const BASE_URL = "https://api.themoviedb.org/3/search/movie";
 
 export const fetchMovies = async (
@@ -10,7 +10,7 @@ export const fetchMovies = async (
 ): Promise<MoviesResponse> => {
   const { data } = await axios.get<MoviesResponse>(BASE_URL, {
     params: {
-      api_key: MY_API_KEY,
+      api_key: API_KEY,
       query,
       page,
     },
